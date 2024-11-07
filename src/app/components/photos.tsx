4fa3bea0625e00b26d66a64e2d05e17802dc65f5
brandoxcode <brandon.a2001@outlook.es>
2024-11-06 22:47:23 -0500

    fix gallery

--- src/app/components/photos.tsx
+++ src/app/components/photos.tsx
@@ -2,19 +2,19 @@
 import Image from "next/image";
 import list from "../../../utils/galleryList";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation } from "swiper/modules";
+import { Navigation } from "swiper/modules";
 import 'swiper/css/bundle'
 
 const photos = () => {
     return (
-        <section className="py-60 md:py-72 lg:max-xl:py-96">
+        <section className="py-72 mt-14 lg:max-xl:py-96">
             <div className="container">
                 <Swiper
                     slidesPerView={3}
-                    spaceBetween={30}
+                    spaceBetween={5}
                     navigation={true}
                     loop={true}
-                    modules={[Autoplay, Navigation]}
+                    modules={[Navigation]}
                     className="h-96 w-full rounded-md"
                 >
                     {list.map((item, index) => (
