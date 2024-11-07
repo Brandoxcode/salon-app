a925e24e7221f1141a609702822ededde4fd4fef
brandoxcode <brandon.a2001@outlook.es>
2024-11-06 22:52:53 -0500

    fix gallery

--- src/app/components/photos.tsx
+++ src/app/components/photos.tsx
@@ -7,32 +7,37 @@ import 'swiper/css/bundle'
 
 const photos = () => {
     return (
-        <section className="py-72 mt-14 lg:max-xl:py-96">
-            <div className="container">
-                <Swiper
-                    slidesPerView={3}
-                    spaceBetween={5}
-                    navigation={true}
-                    loop={true}
-                    modules={[Navigation]}
-                    className="h-96 w-full rounded-md"
-                >
-                    {list.map((item, index) => (
-                        <SwiperSlide key={index}>
-                            <div className="flex items-center justify-center">
-                                <Image
-                                    width={500}
-                                    height={300}
-                                    alt='prueba'
-                                    className="block h-full object-cover rounded-md"
-                                    src={item.img}
-                                />
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-        </section>
+        <>
+            <section className="py-40 md:py-60 lg:max-xl:py-96">
+                <div className="flex justify-center items-center pb-4">
+                    <h1 className="tracking-widest font-medium text-3xl">Check out our photos</h1>
+                </div>
+                <div className="container">
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={5}
+                        navigation={true}
+                        loop={true}
+                        modules={[Navigation]}
+                        className="h-96 w-full rounded-md"
+                    >
+                        {list.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="flex items-center justify-center">
+                                    <Image
+                                        width={500}
+                                        height={300}
+                                        alt='prueba'
+                                        className="block h-full object-cover rounded-md"
+                                        src={item.img}
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
+            </section>
+        </>
     );
 }
 
