ec721b1b2ac5ebef623c2ea834777f1d41e039b7
brandoxcode <brandon.a2001@outlook.es>
2024-12-18 21:34:53 -0500

    lizbeth

--- src/app/components/team.tsx
+++ src/app/components/team.tsx
@@ -59,7 +59,7 @@ const team = () => {
                         alt="Sunset in the mountains"
                     />
                     <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">Liu</div>
+                        <div className="font-bold text-xl mb-2">Lizbeth</div>
                         <p className="text-gray-700 text-base">
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                         </p>
