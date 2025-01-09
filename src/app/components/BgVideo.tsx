0fda77637a72c8bb387bcdae9b1c385aae19f0ab
brandoxcode <brandon.a2001@outlook.es>
2025-01-08 19:30:05 -0500

    new video

--- src/app/components/BgVideo.tsx
+++ src/app/components/BgVideo.tsx
@@ -13,9 +13,6 @@ const bgVideo = () => {
                         loop
                         playsInline
                         controls={false} />
-                    <div className="relative z-10 flex items-center justify-center h-full bg-black bg-opacity-50">
-                        <Welcome />
-                    </div>
                 </div>
             </main>
         </>
