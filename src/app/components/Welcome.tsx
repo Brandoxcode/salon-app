27f742beecfc94a11b3615cb54020bfeedfc326c
brandoxcode <brandon.a2001@outlook.es>
2025-01-12 21:42:40 -0500

    changes first text

--- src/app/components/Welcome.tsx
+++ src/app/components/Welcome.tsx
@@ -2,7 +2,7 @@ const welcome = () => {
 
     return (
         <>
-            <h1 className="tracking-widest font-medium p-[300px] px-0 text-3xl text-white w-20 bg-opacity-50">Blue Bloosom, is all about you!</h1>
+            <h1 className="tracking-widest font-medium p-[300px] px-0 text-3xl text-white w-40 bg-opacity-50">Blue Bloosom Hair Salon, Located in Cape Coral </h1>
         </>
     )
 }
