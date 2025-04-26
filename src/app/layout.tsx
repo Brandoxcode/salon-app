59778f5d677542c779f1decd827c10a68842c729
Brandon Araya <116689478+Brandoxcode@users.noreply.github.com>
2025-04-26 13:36:42 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Blue bloosom",
+  title: "Blue bloosom hair salon",
   description: "Blue Blossom hair salon located in Cape Coral, Florida",
 };
 
